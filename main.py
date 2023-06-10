b3e26c2f9b5546f33dbd4cfcd9831ca46fd94426
Diego Corredeira <91692393+DiegoCorredeira@users.noreply.github.com>
2023-06-10 14:56:02 -0300

    Deixando estrutura mais limpa

--- main.py
+++ main.py
@@ -5,31 +5,28 @@ import tkinter as tk
 from tkinter import messagebox, filedialog
 
 
-def baixar_videos(url, pasta_destino):
-    try:
-        youtube = pytube.YouTube(url)
-        audio = youtube.streams.filter(only_audio=True).first()
-        destino = audio.download(output_path=pasta_destino)
-        nome_mp3 = f"{youtube.title}.mp3"
-        artist = youtube.author
-        nome_destino = os.path.join(pasta_destino, nome_mp3)
-        os.rename(destino, nome_destino)
-        messagebox.showinfo('Sucesso', f"Você baixou uma musica de {artist}")
-        return nome_destino
-    except pytube.exceptions.ExtractError:
-        messagebox.showerror("Erro", "Erro ao extraír audio do vídeo.")
-
-    except Exception as e:
-        messagebox.showerror("Error", f"Ocorreu um erro ao baixar o áudio: {str(e)}")
-
-
 def download_audio():
-    url = entry_url.get()
+    url = entry_url.get().strip()
+    if not url:
+        messagebox.showwarning("Aviso", "Por favor, insira uma URL")
+        return
+
     pasta_destino = filedialog.askdirectory(title='Selecione a pasta de destino')
     if pasta_destino:
-        audio_file = baixar_videos(url, pasta_destino)
-        if audio_file:
-            messagebox.showinfo("Concluído", f'Download concluído com sucesso!')
+        try:
+            youtube = pytube.YouTube(url)
+            audio = youtube.streams.filter(only_audio=True).first()
+            destino = audio.download(output_path=pasta_destino)
+            nome_mp3 = f"{youtube.title}.mp3"
+            artist = youtube.author
+            nome_destino = os.path.join(pasta_destino, nome_mp3)
+            os.rename(destino, nome_destino)
+            messagebox.showinfo('Sucesso', f"Você baixou uma musica de {artist}")
+            return nome_destino
+        except pytube.exceptions.ExtractError:
+            messagebox.showerror("Erro", "Erro ao extraír audio do vídeo.")
+        except Exception as e:
+            messagebox.showerror("Error", f"Ocorreu um erro ao baixar o áudio: {str(e)}")
 
 
 root = tk.Tk()
