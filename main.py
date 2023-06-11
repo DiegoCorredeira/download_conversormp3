ac6eb6b82c3820243bf0b0db05d3a63d9144dc45
Diego Corredeira <91692393+DiegoCorredeira@users.noreply.github.com>
2023-06-10 22:08:36 -0300

    criando conexao com banco de dados

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
+from googleapiclient.discovery import build
 import pytube
 from pytube import exceptions
 import os
+import sqlite3
 import tkinter as tk
 from tkinter import messagebox, filedialog
 
+API_KEY = 'AIzaSyCWJT2WyA1MqdaaXv6kFVo_doBhbpDur8U'
+# Criar a tabela - OK
+# Inserir dados na tabela - OK
+# Obter resposta da tabela - OK
+# Search Integrado com a API do Youtube
+# Historico Embutido no programa com output
+historico = []
+
+def create_table():
+    connection = sqlite3.connect('historico.db')
+    cursor = connection.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS historico (id INTEGER PRIMARY KEY,url TEXT, titulo TEXT)')
+    connection.commit()
+    connection.close()
+
+def insert_data(url, titulo):
+    connection = sqlite3.connect('historico.db')
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO historico (url, titulo) VALUES (?,?)', (url, titulo))
+    connection.commit()
+    connection.close()
+
+def get_data():
+    connection = sqlite3.connect('historico.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM historico')
+    registros = cursor.fetchall()
+    connection.close()
+    return registros
 
 def baixar_audio_yt(url, pasta_destino):
     try:
@@ -13,8 +44,10 @@ def baixar_audio_yt(url, pasta_destino):
         nome_mp3 = f"{youtube.title}.mp3"
         artist = youtube.author
         nome_destino = os.path.join(pasta_destino, nome_mp3)
+        historico.append(nome_mp3)
         os.rename(destino, nome_destino)
         messagebox.showinfo('Sucesso', f"Você baixou uma musica de {artist}")
+
         return nome_destino
     except pytube.exceptions.ExtractError:
         messagebox.showerror("Erro", "Erro ao extraír audio do vídeo.")
@@ -34,12 +67,14 @@ def download_audio():
             messagebox.showinfo("Concluído", f"Download concluído")
 
 
+
 def open_folder():
     pasta_destino = filedialog.askdirectory(title="Selecione a pasta de destino. ")
     if pasta_destino:
         os.startfile(pasta_destino)
 
-
+def exibir_historico():
+    messagebox.showinfo("Histórico de Downloads", "\n".join(historico))
 root = tk.Tk()
 root.title('Download audio do Youtube')
 root.geometry("400x200")
@@ -57,5 +92,8 @@ button_download.pack(pady=10)
 
 button_open_folder = tk.Button(root, text="Abrir Pasta", command=open_folder, font=("Arial, 12"), width=15)
 button_open_folder.pack(pady=10)
+
+button_historico = tk.Button(root, text="Exibir Histórico", command=exibir_historico, font=("Arial, 12"), width=15)
+button_historico.pack(pady=10)
 
 root.mainloop()
